f610c064ea1a99d3ec9dcb0cdb72e9800f21acd9
Huper12345 <huper12345@gmail.com>
2022-10-22 21:17:43 +0300

    add function convert string to number

--- app.js
+++ app.js
@@ -1,66 +1,22 @@
 "use strict";
 
 
-let obj1 = {
-	firstName: "Витя",
-	lastName: "Киселёв",
-	age: 45,
-	children: null,
-	dates: {
-		birthday: '26.01.1977',
-		wedding: '05.09.2000',
-		graduationFromUniversity: '07.06.2003',
-	},
-}
-
-let obj2 = {
-	firstName: "Витя",
-	lastName: "Киселёв",
-	age: 45,
-	children: null,
-	dates: {
-		birthday: '26.01.1977',
-		wedding: '05.09.2000',
-		graduationFromUniversity: {
-            Marina: "Sun",
-            Alena: {
-                nine: "Cat",
-                seven: "Dog",
-            }
-        }
-	},
-    latestTrip: {
-        Minsk: '29.05.2022',
-        Barselona: '25.03.2021',
+function replaceStringNumber(value) {
+    if( value == "") {
+        return 0;
     }
-}
-
-showObject(obj2);
-
-
-function showObject(obj) {
-    let memory = "";
     
-    checkObj(obj);
+    if( typeof value === "string" || value === null || typeof value === "boolean" ) {
+        value = parseInt(value);
+        return value;
+    } 
 
-    function checkObj(objValue) {
-        
-        for (let key in objValue) {
-            
-            if ( objValue[key] == null) {
-                objValue[key] = "null";
-            }
-
-            if (typeof objValue[key] == 'object') {
-                memory = key + " - { ";
-                console.log(memory);
-                checkObj(objValue[key]);
+    else if (Number.isNaN(value)) {
+        return 0;
+    } 
     
-            } else {
-                memory = key + " - " + objValue[key] + " }";
-                console.log(memory);
-            }
-        }
+    else {
+        return "Ошибка, тип данных не строка!"
     }
-}
 
+}
