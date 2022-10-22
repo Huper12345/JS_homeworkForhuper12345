46c338e85a37a4eb116ac586b615d996f725eda0
Huper12345 <huper12345@gmail.com>
2022-10-22 22:25:00 +0300

    update task7

--- app.js
+++ app.js
@@ -2,22 +2,17 @@
 
 
 function replaceStringNumber(value) {
-    if( value === "") {
-        value = Number(value)
-        return value;
-    }
-    
     if( typeof value === "string" || value === null || typeof value === "boolean" ) {
-        value = parseInt(value);
+        value = parseInt(value) || Number(value);
         return value;
     } 
 
-    else if (value !== value) {
+    else if (Object.is(value, NaN)) {
         return 0;
     } 
     
     else {
-        return "Ошибка, тип данных не строка!"
+        return "Что то пошло не так!"
     }
 
 }
