bbb1a94fc87ad18819f083344d8b8d87a5f113e6
Huper12345 <huper12345@gmail.com>
2022-10-26 22:52:04 +0300

    add objects for function

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 "use strict";
 
 
-function replaceStringNumber(value) {
-    return parseFloat(value) || Number(value);
-}
+const person1 = {
+	name: 'Jack Truper',
+    amount: '24500',
+	personal: '5',
+	regional: '2',
+	quantitative: '15'
+};
 
-// А что если использовать Parsefloat? Можно избежать проблемы с числами вроде 1334.222323. 
-// Насколько я понял отличия будут если записать экспоненциальную запись числа 1e5 например.
+const person2 = {
+	name: 'Mick Puper',
+    amount: '27800',
+	personal: '7',
+	regional: '3',
+	quantitative: '17'
+};
+
+const person3 = {
+	name: 'Elly Oldbaby',
+    amount: '23300',
+	personal: '10',
+	regional: '5',
+	quantitative: '13'
+};
+
+const person4 = {
+	name: 'John Black',
+    amount: '22100',
+	personal: '12',
+	regional: '9',
+	quantitative: '20'
+};
+
+const person5 = {
+	name: 'Chang Yellow',
+    amount: '20350',
+	personal: '14',
+	regional: '10',
+	quantitative: '19'
+};
+
